dd72cf6d467373176a408fe770103e2163192c4b
Mehdi Chraibi <chraibimehdi11@gmail.com>
2025-10-28 18:49:14 -0400

    Tab design completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      primary: '#030014',
+      secondary: '#151312',
+      light: {
+        100: '#D6C6FF',
+        200: '#A8B5DB',
+        300: '#9CA4AB'
+      },
+      dark: {
+        100: '#221f3d',
+        200: '#0F0D23'
+      },
+      accent: '#AB8BFF'
+    },
   },
   plugins: [],
 };
